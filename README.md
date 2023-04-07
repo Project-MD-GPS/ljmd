33bceba45fb78a7b48bfa14146a201529fb4bb92
MGisonni <massimo.gisonni@gmail.com>
2023-04-07 12:14:43 +0200

    Hybrid version

--- README.md
+++
@@ -1,1 +0,0 @@
-Starting group project
--- src/energies.c
+++ src/energies.c
@@ -1,13 +1,22 @@
+#include <mdlib-util.h>
 #include <mdlib.h>
 
 /* compute kinetic energy */
 void ekin(mdsys_t *sys)
 {
     int i;
-    
+    double ekin_tmp = 0.0;
     sys->ekin=0.0;
+
+    // execute the loop in parallel
+#ifdef _OPENMP
+    #pragma omp parallel for reduction(+:ekin_tmp)
+#endif
     for (i=0; i<sys->natoms; ++i) {
-        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+        ekin_tmp += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
     }
+
+    // update the global variable
+    sys->ekin = ekin_tmp;
     sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
 }
